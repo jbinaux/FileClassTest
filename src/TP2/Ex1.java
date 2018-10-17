3d6392e7326ed587e39ec016338853869f15cc85
jbinaux <37798129+jbinaux@users.noreply.github.com>
2018-10-17 11:56:53 +0200

    read Number

---
+++ src/TP2/Ex1.java
@@ -0,0 +1,58 @@
+package TP2;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex1 {
+	
+	public static int readNumber(String line)
+	{
+		int number = 0;
+		try
+		{
+			number = Integer.parseInt(line);
+			if(number >= 0 && number <= 20)
+			{
+				return number;
+			}
+			else
+			{
+				System.out.println("entrez un nombre entre 0 et 20");
+				return -1;
+			}
+		}
+		catch (NumberFormatException e)
+		{
+			System.out.println("entrez un nombre");
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String line = null;
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int number = 0;
+		double total = 0;
+		int nbNote = 0;
+		
+		while(!(line = scan.nextLine()).equals("fini"))
+		{
+			if((number = readNumber(line)) != -1)
+			{
+				total += number;
+				nbNote++;
+			}
+		}
+		if(nbNote != 0)
+		{
+			System.out.println("moyenne = " + new DecimalFormat("#.#").format(total / nbNote));
+		}
+		else
+		{
+			System.out.println("entrez au moins une note !");
+		}
+	}
+	
+}
